f8836cd6da5a463f7baddba7f6444b878ef60176
Devsharma1710 <d4dev1710@gmail.com>
2022-08-09 16:36:18 +0530

    assignment

---
+++ src/routes/intro.js
@@ -0,0 +1,7 @@
+const batchName = "plutonium"  
+
+let printName = function(){
+    console.log('bathc name is',batchName )
+}
+ module.exports.name =batchName
+ module.exports.printName = printName
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,18 +2,18 @@ const express = require('express');
 const abc = require('../introduction/intro')
 const router = express.Router(); 
 
-// router.get('/test-me', function (req, res) {
-//     console.log('My batch is', abc.name)
-//     abc.printName()
-//     logger.welcome()
+router.get('/test-me', function (req, res) {
+    console.log('My batch is', abc.name)
+    abc.printName()
+    logger.welcome()
 
-//     res.send('My second ever api!')
-// });
+    res.send('My second ever api!')
+});
 
-// router.get('/students', function (req, res){
-//     let students = ['Sabiha', 'Neha', 'Akash']
-//     res.send(students)
-// })
+router.get('/students', function (req, res){
+    let students = ['Sabiha', 'Neha', 'Akash']
+    res.send(students)
+})
 
 
       ///first problems starts here 
@@ -64,7 +64,7 @@ const router = express.Router();
 
 
 
-// router.get('/student-details/:name', function(req, res){
+router.get('/student-details/:name', function(req, res){
     /*
     params is an attribute inside request that contains 
     dynamic values.
@@ -73,15 +73,15 @@ const router = express.Router();
     and value is what is sent in the request
     */
 
-    // let requestParams = req.params
+    let requestParams = req.params
 
     // JSON strigify function helps to print an entire object
     // We can use any ways to print an object in Javascript, JSON stringify is one of them 
-//     console.log("This is the request "+ JSON.stringify(requestParams))
-//     let studentName = requestParams.name
-//     console.log('Name of the student is ', studentName)
+    console.log("This is the request "+ JSON.stringify(requestParams))
+    let studentName = requestParams.name
+    console.log('Name of the student is ', studentName)
     
-//     res.send('Dummy response') 
-// })
+    res.send('Dummy response') 
+})
 
-// module.exports = router;     
+module.exports = router;    
